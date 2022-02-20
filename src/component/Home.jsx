eb8ae298e48e86331e70d4cef9cad7df809f487e
unknown <you@example.com>
2022-02-20 14:36:30 +0100

    initial stage

---
+++ src/component/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+<div className="row">
+    <div className="mango col-6 ">
+        <div className="container">
+        <h5 class="card-title  display-3 fw-bolder mt-5 mb-0">Best croc shoe in town</h5>
+        <div class="card bg-white text-black">
+        
+          <p class="card-text border-0 lead fx-2">Check out the slickest of croc in different colour</p>
+         
+        </div>
+        </div>
+    </div>
+    <div className="mang col-6">
+        <img class="title-image" src="/assestss/crocl.jpg" class="card-img" alt="background" height="400px"/>
+    </div>
+</div>
+    );
+}
+
+export default Home;
