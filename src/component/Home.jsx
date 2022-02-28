bca08366cf1799550ab6d182572089ed72b980ff
unknown <you@example.com>
2022-02-28 21:31:45 +0100

    stage 3

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 const Home = () => {
     return (
-<div className="row">
-    <div className="mango col-6 ">
-        <div className="container">
-        <h5 class="card-title  display-3 fw-bolder mt-5 mb-0">Best croc shoe in town</h5>
-        <div class="card bg-white text-black">
+<div className="container-fluid"> 
+    <div className="row">
+    
+        <div className="mango col-6 ">
+       
+            <h5 class="card-title  display-3 fw-bolder mt-5  mb-0">Best croc shoe in town</h5>
+            <div class="card bg-white text-black">
         
-          <p class="card-text border-0 lead fx-2">Check out the slickest of croc in different colour</p>
+             <p class="card-text lead fx-2 ">Check out the slickest of croc in different colour</p>
          
+            </div>
         </div>
-        </div>
+    
+        <div className="mang col-6">
+        <img class="title-image" src="/assestss/crockus.jpg" class="card-img" alt="background" height="500px"/>
     </div>
-    <div className="mang col-6">
-        <img class="title-image" src="/assestss/crocl.jpg" class="card-img" alt="background" height="400px"/>
     </div>
 </div>
     );
---
+++ src/component/Product.jsx
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react';
+
+const Product = () => {
+    const [state, setstate] = useState(initialState);
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Product;
